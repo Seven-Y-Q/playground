86b0d8e0c997421d31b87e88702dd06bd2b01015
guoyaqi <15902999528@163.com>
2020-03-22 20:06:38 +0800

    support typer

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,9 +1,19 @@
-import html from './base64/template.txt';
-import css from './base64/css.txt';
-import js from './base64/js.txt';
+import htmlBase64 from './base64/template.txt';
+import cssBase64 from './base64/css.txt';
+import jsBase64 from './base64/js.txt';
 
-export {
-  js,
-  css,
-  html
-}
+import htmlMoon from './moon/template.txt';
+import cssMoon from './moon/css.txt';
+import jsMoon from './moon/js.txt';
+
+export const base64 = {
+  js: jsBase64,
+  css: cssBase64,
+  html: htmlBase64
+};
+
+export const moon = {
+  js: jsMoon,
+  css: cssMoon,
+  html: htmlMoon
+};
